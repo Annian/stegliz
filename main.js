c8c8181c521479f7a053d7b39639607322a90bb1
Asurias <anihakobyan@rocketmail.com>
2018-04-10 18:05:26 +0400

    finally it works

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+
+  /* Open Menu */
+	$( ".open-close-btn" ).on('click touchstart', function(e) {
+		// prevent default anchor click 
+		e.preventDefault();
+		$(".overlay").toggleClass("overlay-open");
+		$("#hamburger-icon").toggleClass("hamburger-open");
+	});
